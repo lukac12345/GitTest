c78c000c829bf4ade41fa7ea4ff09070d1212d3a
NTDOMAIN\pavol <pavol.lukac@prosoft.sk>
2024-01-29 21:57:03 +0100

    prvy commit

---
+++ testdata/data.js
@@ -0,0 +1,25 @@
+export const dataLogin = {
+    loginInvalid: 'P130a',
+    passwordInvalid: 'bbbbb',
+    loginValid: 'P130',
+    passwordValid: 'aaaaa',
+    baseURL: 'https://dockerdev.prosoft.sk:8083/'
+}
+
+export const dataZamestnanci = {
+    meno: 'Ivan',
+    priezvisko: 'Lapin',
+    titulPred: 'Doc.',
+    titulZa: 'PhD.',
+    zsskID: '1',    
+}
+
+export const dataZakaznici = {
+    meno: 'Peter',
+    dokladID: 'DD123456',
+    
+}
+
+export const dataBlacklist = {
+    nazovCD: 'Cest.doklad',    
+}
